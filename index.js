4b6b425099c547b5bed5fd93eaab0e971192b7d3
Divyanshu <developer.idblfs@gmail.com>
2019-10-13 14:00:18 +0530

    webhook for accunt

--- index.js
+++ index.js
@@ -19,13 +19,15 @@ app.get('/webhook', (req, res) => {
     // Your verify token. Should be a random string.
 
     // Parse the query params
+    console.log('step 1');
     let mode = req.query['hub.mode'];
     let token = req.query['hub.verify_token'];
     let challenge = req.query['hub.challenge'];
 
     // Checks if a token and mode is in the query string of the request
     if (mode && token) {
-
+        console.log('step 2');
+    
         // Checks the mode and token sent is correct
         if (mode === 'subscribe' && token === VERIFY_TOKEN) {
 
@@ -35,6 +37,8 @@ app.get('/webhook', (req, res) => {
 
         } else {
             // Responds with '403 Forbidden' if verify tokens do not match
+            console.log('NOT_VERIFIED');
+    
             res.sendStatus(403);
         }
     }
@@ -42,15 +46,18 @@ app.get('/webhook', (req, res) => {
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {
-
+    console.log('STEP -1 APP');
+    
     // Parse the request body from the POST
     let body = req.body;
-
+    
     // Check the webhook event is from a Page subscription
     if (body.object === 'page') {
-
+        console.log('STEP 2 APP');
+    
         body.entry.forEach(function (entry) {
-
+            console.log('steps');
+        
             // Gets the body of the webhook event
             let webhook_event = entry.messaging[0];
             console.log(webhook_event);
@@ -75,7 +82,10 @@ app.post('/webhook', (req, res) => {
 
     } else {
         // Return a '404 Not Found' if event is not from a page subscription
+        console.log('STEP 2 NOT WORKED');
+    
         res.sendStatus(404);
+
     }
 
 });
@@ -141,6 +151,7 @@ function handlePostback(sender_psid, received_postback) {
 }
 
 function callSendAPI(sender_psid, response) {
+    let access_token
     // Construct the message body
     let request_body = {
         "recipient": {
@@ -152,7 +163,7 @@ function callSendAPI(sender_psid, response) {
     // Send the HTTP request to the Messenger Platform
     request({
         "uri": "https://graph.facebook.com/v2.6/me/messages",
-        "qs": { "access_token": VERIFY_TOKEN },
+        "qs": { "access_token": access_token },
         "method": "POST",
         "json": request_body
     }, (err, res, body) => {
