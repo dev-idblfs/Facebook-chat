15d257e7be5312aa9c6d4df753ccef89724171d7
Divyanshu <dev-idblfs@users.noreply.github.com>
2019-10-24 11:27:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,36 +10,5 @@ app.listen(process.env.PORT || 1337, () => console.log("server is running...@ " 
 // Adds support for GET requests to our webhook
 app.get('/', (req, res) => {
     console.log('working');
-    fbAsyncInit = function () {
-        FB.init({
-            appId: 2605373396193317,
-            cookie: true,
-            xfbml: true,
-            version: 'v4.0'
-        });
-
-        FB.AppEvents.logPageView();
-
-    };
-
-    if (typeof d !== 'undefined') {
-        (function (d, s, id) {
-            console.log('aa');
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) { return; }
-            js = d.createElement(s); js.id = id;
-            js.src = "https://connect.facebook.net/en_US/sdk.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }
-            (document, 'script', 'facebook-jssdk'));
-
-
-    }
-
-    FB.getLoginStatus(function (response) {
-        statusChangeCallback(response);
-    });
-
-
     res.sendStatus(200);
 });
